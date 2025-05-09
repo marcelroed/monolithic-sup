402b5df7abf6cd44ad83e101874f9376024a8532
Marcel Rød <marcelroed@gmail.com>
2025-05-09 12:55:30 -0700

    Generate a minimal model with gradients using Jax

--- construct_jaxpr.py
+++ construct_jaxpr.py
@@ -19,13 +19,44 @@ def attn_simple(x):
     return attn(x, x, x)
 
 
+class MinimalModel:
+    @staticmethod
+    def construct():
+        weights = {
+            'linear': jax.random.normal(jax.random.PRNGKey(0), (4, 8)),
+        }
+        return weights
+    
+    @staticmethod
+    def forward(weights, x):
+        x = jnp.dot(x, weights['linear'])
+        return x
+
+    @staticmethod
+    def loss(weights, x, y):
+        # input = jnp.ones((4,))
+        # output = jax.random.normal(jax.random.PRNGKey(0), (8,))
+        prediction = MinimalModel.forward(weights, x)
+        return jnp.mean((prediction - y) ** 2)
+
+
 if __name__ == "__main__":
-    inp = jnp.ones((1, 4, 8))
-    out, f_derivative = jax.vjp(attn_simple, inp)
+    # inp = jnp.ones((1, 4, 8))
+    # out, f_derivative = jax.vjp(attn_simple, inp)
 
-    f_jaxpr = jax.make_jaxpr(attn_simple)(inp)
-    f_derivative_jaxpr = jax.make_jaxpr(f_derivative)(inp)
+    # f_jaxpr = jax.make_jaxpr(attn_simple)(inp)
+    # f_derivative_jaxpr = jax.make_jaxpr(f_derivative)(inp)
 
-    print("JAXPR representation of f:")
-    print(f_jaxpr)
-    print(f_derivative_jaxpr)
+    # print("JAXPR representation of f:")
+    # print(f_jaxpr)
+    # print(f_derivative_jaxpr)
+
+    weights = MinimalModel.construct()
+
+    loss_and_grad = jax.value_and_grad(MinimalModel.loss)
+
+    x = jax.random.normal(jax.random.PRNGKey(0), (4,))
+    y = jax.random.normal(jax.random.PRNGKey(1), (8,))
+
+    model_jaxpr = jax.make_jaxpr(loss_and_grad)(weights, x, y)
+    print(model_jaxpr)
