e82d4505605ee7ce299e0ca887d51127401c8eaf
Herman Brunborg <herman@brunborg.com>
2025-05-10 21:27:48 -0700

    making the constants clearer

--- tinystory.py
+++ tinystory.py
@@ -7,33 +7,35 @@ VAL_DATA = Path("/data/brunborg/tokenized/tinystory-valid.npy")
 
 VOCAB_SIZE = 10_000
 
-embedding_dim = 512
-context_length = 128
+EMBEDDING_DIM = 512
+CONTEXT_LENGTH = 128
+SEED = 42
 
-np.random.seed(42)
-embeddings = np.random.randn(VOCAB_SIZE, embedding_dim)
+np.random.seed(SEED)
+
+embeddings = np.random.randn(VOCAB_SIZE, EMBEDDING_DIM)
 
 train_dataset = np.load(TRAIN_DATA)
 val_dataset = np.load(VAL_DATA)
 
 def get_batch(
     dataset: npt.NDArray,
-    context_length: int,
+    CONTEXT_LENGTH: int,
 ):
-    starting_idxs = np.random.randint(0, VOCAB_SIZE, (context_length,))
+    starting_idxs = np.random.randint(0, VOCAB_SIZE, (CONTEXT_LENGTH,))
     x = np.stack([
-            dataset[i : i + context_length]
+            dataset[i : i + CONTEXT_LENGTH]
             for i in starting_idxs
     ])
     y = np.stack(
         [
-            dataset[i + 1 : i + 1 + context_length]
+            dataset[i + 1 : i + 1 + CONTEXT_LENGTH]
             for i in starting_idxs
         ]
     )
     return embeddings[starting_idxs,:], starting_idxs
 
-def get_loader(dataset, context_length=context_length):
+def get_loader(dataset, context_length=CONTEXT_LENGTH):
     while True:
         yield get_batch(dataset, context_length)
 
